4eb8f792bf46037538d2fba62f3bfa0b6ebbfb27
Joshua Novak <josh19novak@gmail.com>
2015-08-08 21:24:49 -0500

    Assignment 23

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Joshua on 8/8/15.
+//  Copyright (c) 2015 Joshua Novak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/BlocBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlocBrowser
+//
+//  Created by Joshua on 8/8/15.
+//  Copyright (c) 2015 Joshua Novak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
