3617f7f8cea1d6a8987db8b901fb2f57da4c8e01
Joshua Novak <josh19novak@gmail.com>
2015-08-13 11:39:18 -0500

    Assignment 25 Toolbar moved

--- BlocBrowser/BlocBrowser/AppDelegate.m
+++ BlocBrowser/BlocBrowser/AppDelegate.m
@@ -22,7 +22,6 @@
     self.window.backgroundColor = [UIColor whiteColor];
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
     
-    
     [self.window makeKeyAndVisible];
     return YES;
 }
--- BlocBrowser/BlocBrowser/ViewController.m
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -102,30 +102,31 @@
     [super viewWillLayoutSubviews];
     
     //make the webview fill main view
-    //self.webView.frame = self.view.frame;
+    self.webView.frame = self.view.frame;
     
     //calculate dimensions
     static const CGFloat itemHeight = 50;
     CGFloat width = CGRectGetWidth(self.view.bounds);
     CGFloat browserHeight = CGRectGetHeight(self.view.bounds) - itemHeight;
+    CGFloat customLabelHeight = 80;
     
     //assign the frames
     self.textField.frame = CGRectMake(0, 0, width, itemHeight);
     self.webView.frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), width, browserHeight);
     
-    self.awesomeToolbar.frame = CGRectMake(20, 100, 280, 60);
+    self.awesomeToolbar.frame = CGRectMake(0, CGRectGetHeight(self.view.bounds)- customLabelHeight, width, customLabelHeight);
 }
 
 #pragma mark - AwesomeFloatingToolbarDelegate
 
 -(void) floatingToolbar:(AwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title {
-    if ([title isEqual:NSLocalizedString(@"Back", @"Back command")]) {
+    if ([title isEqual:kWebBrowserBackString]) {
         [self.webView goBack];
-    }else if ([title isEqual:NSLocalizedString(@"Forward", @"Forward command")]) {
+    }else if ([title isEqual:kWebBrowserForwardString]) {
         [self.webView goForward];
-    }else if ([title isEqual:NSLocalizedString(@"Stop", @"Stop command")]) {
+    }else if ([title isEqual:kWebBrowserStopString]) {
         [self.webView stopLoading];
-    }else if ([title isEqual:NSLocalizedString(@"Refresh", @"Reload command")]){
+    }else if ([title isEqual:kWebBrowserRefreshString]){
         [self.webView reload];
     }
 }
